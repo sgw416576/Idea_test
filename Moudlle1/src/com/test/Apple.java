987fcabfb4950b81f2196186f3caac43df2b643b
sgw416576 <1196476391@qq.com>
2021-01-21 22:01:17 +0800

    提交的第一个idea文件

---
+++ Moudlle1/src/com/test/Apple.java
@@ -0,0 +1,16 @@
+package com.test;
+
+/**
+ * @Auther: songguowei
+ * @Date: 2021/1/21 - 01 - 21 - 21:54
+ * @Description: com.test
+ * @version: 1.0
+ */
+public class Apple {
+    //我建立一个main方法，可还行？
+    public static void main(String[] args) {
+        String str ="git测试一下";
+        System.out.println(str);
+    }
+
+}
